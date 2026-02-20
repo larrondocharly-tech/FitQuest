195734066e6db50c13868c2b319a04cfc6d31cb8
larrondocharly-tech <larrondo.charly@gmail.com>
2026-02-20 23:43:36 +0100

    check point works 1 pager

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // si tu as des images externes plus tard, tu pourras ajouter images.domains ici
+};
+
+module.exports = nextConfig;
